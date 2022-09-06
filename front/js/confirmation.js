38275d66fc09ccdf1315af10fe3f61965810f96a
Hugo Cadenat <h.cadenat@gmail.com>
2022-09-06 16:47:11 +0200

    Création de mon fetch POST permettant d'envoyer les données du formulaire ainsi que les produits sur mon API. Attention : problème pour envoyer les données sur l'api/order... Faut trouver ce qui bloque à ce niveau

---
+++ front/js/confirmation.js
@@ -0,0 +1,5 @@
+// Récupérer les infos de Contact sur le local storage
+// Une fois la commande validée, faire une redirection
+// vers la page Confirmation
+
+// 
--- front/js/library/form.js
+++ front/js/library/form.js
@@ -1,32 +1,66 @@
 const form = document.querySelector('.cart__order__form');
 const regExpName = /^[a-zA-ZàáâäãåąčćęèéêëėįìíîïłńòóôöõøùúûüųūÿýżźñçčšžÀÁÂÄÃÅĄĆČĖĘÈÉÊËÌÍÎÏĮŁŃÒÓÔÖÕØÙÚÛÜŲŪŸÝŻŹÑßÇŒÆČŠŽ∂ð ,\.'-]+$/u;
+const regExpAddress = /^\s*\S+(?:\s+\S+){2}$/;
+const regExpCity = /^([a-zA-Z\u0080-\u024F]+(?:. |-| |'))*[a-zA-Z\u0080-\u024F]*$/;
 const regExpEmail = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/;
+
+
+// Ajout de conditions sur chaque input du formulaire
+// Avec des regExp
 
 form.firstName.addEventListener('change', () => {
     if (regExpName.test(form.firstName.value)) {
+        document.getElementById('firstNameErrorMsg').style.display = "none";
     } else {
-        alert("Le prénom saisi n'est pas valide");
+        document.getElementById('firstNameErrorMsg').style.display = "inline";
+        document.getElementById('firstNameErrorMsg').innerText = "Le prénom saisi n'est pas valide";
     }
 });
 
 form.lastName.addEventListener('change', () => {
     if (regExpName.test(form.lastName.value)) {
+        document.getElementById('lastNameErrorMsg').style.display = "none";
     } else {
-        alert("Le nom saisi n'est pas valide");
+        document.getElementById('lastNameErrorMsg').style.display = "inline";
+        document.getElementById('lastNameErrorMsg').innerText = "Le nom saisi n'est pas valide";
+    }
+});
+
+form.address.addEventListener('change', () => {
+    if (regExpAddress.test(form.address.value)) {
+        // document.getElementById('addressErrorMsg').innerText = "";
+        document.getElementById('addressErrorMsg').style.display = "none";
+    } else {
+        document.getElementById('addressErrorMsg').style.display = "inline";
+        document.getElementById('addressErrorMsg').innerText = "L'adresse saisie n'est pas valide";
+    }
+});
+
+form.city.addEventListener('change', () => {
+    if (regExpCity.test(form.city.value)) {
+        document.getElementById('cityErrorMsg').style.display = "none";
+    } else {
+        document.getElementById('cityErrorMsg').style.display = "inline";
+        document.getElementById('cityErrorMsg').innerText = "La ville saisie n'est pas valide";
     }
 });
 
 form.email.addEventListener('change', () => {
     if (regExpEmail.test(form.email.value)) {
+        document.getElementById('emailErrorMsg').style.display = "none";
     } else {
-        alert("L'adresse email saisie n'est pas valide");
+        document.getElementById('emailErrorMsg').style.display = "inline";
+        document.getElementById('emailErrorMsg').innerText = "L'adresse email saisie n'est pas valide";
     }
 });
+
+
+/////////////////////////////////////////////////////////////
 
 document.getElementById('order').addEventListener('click', (event) => {
     event.preventDefault();
 
-    if ((regExpName.test(form.firstName.value)) && (regExpName.test(form.lastName.value)) && (regExpEmail.test(form.email.value))) {
+    if ((regExpName.test(form.firstName.value)) && (regExpName.test(form.lastName.value)) && (regExpAddress.test(form.address.value)) && (regExpCity.test(form.city.value)) && (regExpEmail.test(form.email.value))) {
         addContact({
             "prénom": form.firstName.value,
             "nom": form.lastName.value,
@@ -40,6 +74,43 @@ document.getElementById('order').addEventListener('click', (event) => {
         alert("Il y a un problème");
     }
 });
+
+
+
+const body = {
+    contact: {
+        prénom: form.firstName.value,
+        nom: form.lastName.value,
+        adresse: form.address.value,
+        ville: form.city.value,
+        email: form.email.value
+    },
+    products: getCart()
+};
+
+
+const setOrder = fetch("https://jsonplaceholder.typicode.com/users", {
+    method: "POST",
+    body: JSON.stringify(body),
+    headers: {
+        "Content-Type": "application/json"
+    }
+});
+
+setOrder.then(async (response) => {
+    try {
+        const content = await response.json();
+        console.log(content);
+        content.products.forEach((product) => {
+            console.log(product.id);
+        });
+    } catch (error) {
+        console.log(error);
+    }
+})
+
+
+
 
 
 // form.email.addEventListener('change', () => {
