b6915b8ac3b62c8d914badb41e76edeb2b76122f
Hugo Cadenat <h.cadenat@gmail.com>
2022-09-25 17:43:56 +0200

    modification implémentation éléments sur page accueil

--- front/js/script.js
+++ front/js/script.js
@@ -6,17 +6,40 @@ const showProducts = async () => {
     const listProducts = await getRequest(productsUrl);
     let cardsHTML = "";
 
-    listProducts.forEach((product) => {
-        cardsHTML += `
-        <a href="./product.html?id=${product._id}">
-            <article>
-                <img src="${product.imageUrl}" alt=${product.altTxt}>
-                    <h3 class="productName">${product.name}</h3>
-                    <p class="productDescription">${product.description}</p>
-            </article>
-        </a>`;
-    });
-    items.innerHTML = cardsHTML;
+    // listProducts.forEach((product) => {
+    //     cardsHTML += `
+    //     <a href="./product.html?id=${product._id}">
+    //         <article>
+    //             <img src="${product.imageUrl}" alt=${product.altTxt}>
+    //                 <h3 class="productName">${product.name}</h3>
+    //                 <p class="productDescription">${product.description}</p>
+    //         </article>
+    //     </a>`;
+    // });
+    // items.innerHTML = cardsHTML;
+
+
+    const addElements = () => {
+        listProducts.forEach((product) => {
+            const items = document.getElementById('items');
+            const newLink = document.createElement('a');
+            newLink.setAttribute('href', `./product.html?id=${product._id}`);
+
+            const newArticle = document.createElement('article');
+            const newContent = newArticle.innerHTML;
+
+            newArticle.innerHTML =
+                `<img src = "${product.imageUrl}" alt = ${product.altTxt}>
+                <h3 class="productName">${product.name}</h3>
+                <p class="productDescription">${product.description}</p>`;
+
+            newLink.appendChild(newArticle);
+            items.appendChild(newLink);
+        });
+    };
+
+    addElements();
 }
 
 showProducts()
+
